d5210f343e7e62349e3f8e0a9cbaa414a0c2b514
TAKUYA\Linda <linda.c218@gmail.com>
2021-05-07 00:06:10 +1200

    add mock created by mockgen

--- a-restful/controllers/command_controller.go
+++ a-restful/controllers/command_controller.go
@@ -2,25 +2,25 @@ package controller
 
 import (
 	"net/http"
+	robotsdk "robot-api-golang/a-restful/interface"
+	util "robot-api-golang/a-restful/utils"
 )
 
 var GetCommandStatus = func(w http.ResponseWriter, r *http.Request) {
 	status := robotsdk.Robot.CurrentState()
-	// resp := util.Message(true, status)
-	// resp["data"] = users
-	// util.Respond(w, resp)
+	resp := util.Message(true, status)
+	resp["status"] = "robot status is g"
+	util.Respond(w, resp)
 }
 
 var PostCommands = func(w http.ResponseWriter, r *http.Request) {
-	status := robotsdk.Robot.EnqueueTask()
-	// resp := util.Message(true, "Successful")
-	// resp["data"] = users
-	// util.Respond(w, resp)
+	robotsdk.Robot.EnqueueTask()
+	resp := util.Message(true, "Successful - queue command")
+	util.Respond(w, resp)
 }
 
 var PostCancelCommands = func(w http.ResponseWriter, r *http.Request) {
-	status := robotsdk.Robot.CancelTask()
-	// resp := util.Message(true, "Successful")
-	// resp["data"] = users
-	// util.Respond(w, resp)
+	robotsdk.Robot.CancelTask()
+	resp := util.Message(true, "Successful - canceled command")
+	util.Respond(w, resp)
 }
